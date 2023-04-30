a439c0c50454154889b80ad359659ca4ebc00e70
Richard Bignell <bignellrp@gmail.com>
2023-04-30 15:31:47 +0100

    Initial Commit

---
+++ footywatch Watch App/footywatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  footywatchApp.swift
+//  footywatch Watch App
+//
+//  Created by Rik Bignell on 30/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct footywatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
