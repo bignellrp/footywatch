e9be257299bdad7353f7ca5f578224f0814a8526
Richard Bignell <bignellrp@gmail.com>
2023-09-30 18:52:19 +0100

    Added a long press menu

--- footywatch Watch App/ContentView.swift
+++ footywatch Watch App/ContentView.swift
@@ -1,30 +1,133 @@
-//
-//  ContentView.swift
-//  footywatch Watch App
-//
-//  Created by Rik Bignell on 30/04/2023.
-//
-
 import SwiftUI
 
+struct Player {
+    var name: String
+}
+
 struct ContentView: View {
-    @State var count: Int = 0
-    let theWords = ["Pete","Rik","Chris","Cal","Joe","Harry","Louis","Oli","Ben","Lily"]
-    
-    let columns: [GridItem] =
-             Array(repeating: .init(.flexible()), count: 2)
-    
+    @State var teamacount: Int = 0
+    @State var teambcount: Int = 0
+    @State var countsA = Array(repeating: 0, count: 5)
+    @State var countsB = Array(repeating: 0, count: 5)
+    @State var showingResetView = false
+
+    let playersA = [Player(name: "Rik"), Player(name: "Joe"), Player(name: "Cal"), Player(name: "Pete"), Player(name: "Arun")]
+    let playersB = [Player(name: "Darren"), Player(name: "Phil"), Player(name: "Josh"), Player(name: "Ollie"), Player(name: "Mark")]
+
     var body: some View {
-            LazyVGrid(columns: columns) {
-                ForEach(theWords, id: \.self) { word in
-                    Button(action:{self.count += 1})
-                    { Text(word)}.buttonStyle(PlainButtonStyle())
+        NavigationView {
+            ScrollView {
+                HStack(spacing: 20) {
+                    ScrollView {
+                        Text("TeamA: \(teamacount)")
+                        
+                        ForEach(playersA.indices, id: \.self) { index in
+                            Button(action:
+                                {
+                                countsA[index] += 1
+                                self.teamacount += 1
+                            }
+                            )
+                            { Text("\(playersA[index].name) \(countsA[index])")}.buttonStyle(PlainButtonStyle())
+                        }
+                        
+                    }
+                    
+                    ScrollView {
+                        Text("TeamB: \(teambcount)")
+                        
+                        ForEach(playersB.indices, id: \.self) { index in
+                            Button(action:
+                                {
+                                countsB[index] += 1
+                                self.teambcount += 1
+                            }
+                            )
+                            { Text("\(playersB[index].name) \(countsB[index])")}.buttonStyle(PlainButtonStyle())
+                        }
+                        
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                
+                NavigationLink(destination: ResetView(teamACount: $teamacount, teamBCount: $teambcount), isActive: $showingResetView) {
+                  EmptyView()
                 }
             }
-        VStack {
-            Text("TeamA Score: \(count)")
-        }
+            .onLongPressGesture(minimumDuration: 2) {
+                self.showingResetView = true
+            }
         
+            Button(action: {
+                self.teamacount = 0
+                self.teambcount = 0
+            }) {
+                Text("Reset")
+            }
+        }
+        .onAppear {
+            loadTeama()
+            loadTeamB()
+        }
+    }
+
+    func loadTeama() {
+        guard let url = URL(string: "https://footyappdev.richardbignell.co.uk/teama.txt") else {
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                return
+            }
+
+            if let fileContents = String(data: data, encoding: .utf8) {
+                let lines = fileContents.components(separatedBy: "\n")
+                // update UI on the main thread 
+                DispatchQueue.main.async {
+                    // Update your UI here
+                }
+            }
+        }.resume()
+    }
+    
+    func loadTeamB() {
+        guard let url = URL(string: "https://footyappdev.richardbignell.co.uk/teamb.txt") else {
+            return
+        }
+
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                return
+            }
+
+            if let fileContents = String(data: data, encoding: .utf8) {
+                let lines = fileContents.components(separatedBy: "\n")
+                
+                // update UI on the main thread 
+                DispatchQueue.main.async {
+                    // Update your UI here
+                }
+            }
+        }.resume()
+    }
+}
+
+struct ResetView: View {
+    @Binding var teamACount: Int
+    @Binding var teamBCount: Int
+    
+    var body: some View {
+        VStack {
+            Text("You can reset your scores here.")
+            
+            Button(action: {
+                self.teamACount = 0
+                self.teamBCount = 0
+            }) {
+                Text("Reset")
+            }
+        }
     }
 }
 
