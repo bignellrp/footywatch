71714d7819c16c1dacda9f96d664347e5978d9c5
Richard Bignell <bignellrp@gmail.com>
2023-10-01 07:20:48 +0100

    Added boxes around the team scores

--- footywatch Watch App/ContentView.swift
+++ footywatch Watch App/ContentView.swift
@@ -5,6 +5,7 @@ struct Player {
 }
 
 struct ContentView: View {
+
     @State var teamacount: Int = 0
     @State var teambcount: Int = 0
     @State var countsA = Array(repeating: 0, count: 5)
@@ -17,97 +18,55 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-                VStack(spacing: 20) { // changed from HStack to VStack
-                    ScrollView {
-                        //Text("TeamA: \(teamacount)")
-                        // Total Scores
-                        Text("Team A Score: \(self.teamacount)")
+                VStack(spacing: 20) { 
+                    
+                    HStack {
+                        Text("Team A")
                             .font(.title)
-                        Text("Team B Score: \(self.teambcount)")
+                            .foregroundColor(.white)
+                            
+                        Text("Score: \(self.teamacount)")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                    }
+                    .padding(.init(top: 3, leading: 8, bottom: 3, trailing: 8)) // Adjust padding here
+                    .background(Color.red)
+                    .cornerRadius(10)
+
+                    HStack {
+                        Text("Team B")
                             .font(.title)
-
-                        // For Team A
-                        ForEach(playersA.indices, id: \.self) { index in
-                            HStack {
-                                Button(action: {
-                                    if countsA[index] > 0 {
-                                        countsA[index] -= 1
-                                        self.teamacount -= 1
-                                    }
-                                }) {
-                                    Image(systemName: "minus.circle")
-                                }
-                                .buttonStyle(PlainButtonStyle())
-                                .background(Color.red)
-
-                                VStack(alignment: .leading) {
-                                    Text("\(playersA[index].name)").font(.title)
-                                    Text("Score: \(countsA[index])").font(.headline)
-                                }
-
-                                Button(action: {
-                                    countsA[index] += 1
-                                    self.teamacount += 1
-                                }) {
-                                    Image(systemName: "plus.circle")
-                                }
-                                .buttonStyle(PlainButtonStyle())
-                                .background(Color.red)
-                            }
-                        }
-
+                            .foregroundColor(.white)
+                            
+                        Text("Score: \(self.teambcount)")
+                            .font(.headline)
+                            .foregroundColor(.white)
                     }
+                    .padding(.init(top: 3, leading: 8, bottom: 3, trailing: 8)) // Adjust padding here
+                    .background(Color.blue)
+                    .cornerRadius(10)
 
-                    ScrollView {
-                        //Text("TeamB: \(teambcount)")
 
-                        // For Team B
-                        ForEach(playersB.indices, id: \.self) { index in
-                            HStack {
-                                Button(action: {
-                                    if countsB[index] > 0 {
-                                        countsB[index] -= 1
-                                        self.teambcount -= 1
-                                    } 
-                                }) {
-                                    Image(systemName: "minus.circle")
-                                }
-                                .buttonStyle(PlainButtonStyle())
-                                .background(Color.blue)
-
-                                VStack(alignment: .leading) {
-                                    Text("\(playersB[index].name)").font(.title)
-                                    Text("Score: \(countsB[index])").font(.headline)
-                                }
-
-                                Button(action: {
-                                    countsB[index] += 1
-                                    self.teambcount += 1
-                                }) {
-                                    Image(systemName: "plus.circle")
-                                }
-                                .buttonStyle(PlainButtonStyle())
-                                .background(Color.blue)
-                            }
-                        }
-
+                    Spacer().frame(height: 30)
+                    
+                    ForEach(playersA.indices, id: \.self) { index in
+                        playerView(player: playersA[index], count: $countsA[index], teamCount: $teamacount, color: Color.red)
                     }
-                }
-                .frame(maxWidth: .infinity)
+                    
+                    Spacer().frame(height: 30)
 
-                NavigationLink(destination: ResetView(teamACount: $teamacount, teamBCount: $teambcount), isActive: $showingResetView) {
+                    ForEach(playersB.indices, id: \.self) { index in
+                        playerView(player: playersB[index], count: $countsB[index], teamCount: $teambcount, color: Color.blue)
+                    }
+                    
+                }.frame(maxWidth: .infinity)
+
+                NavigationLink(destination: ResetView(teamACount: $teamacount, teamBCount: $teambcount, countsA: $countsA, countsB: $countsB), isActive: $showingResetView) {
                     EmptyView()
                 }
             }
             .onLongPressGesture(minimumDuration: 2) {
                 self.showingResetView = true
-            }
-
-            Button(action: {
-                self.teamacount = 0
-                self.teambcount = 0
-            }) {
-                Text("Reset")
             }
         }
         .onAppear {
@@ -116,40 +75,60 @@ struct ContentView: View {
         }
     }
 
-    func loadTeama() {
-        guard let url = URL(string: "https://footyappdev.richardbignell.co.uk/teama.txt") else {
-            return
-        }
-
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data else {
-                return
+    func playerView(player: Player, count: Binding<Int>, teamCount: Binding<Int>, color: Color) -> some View {
+        return HStack {
+            Button(action: {
+                if count.wrappedValue > 0 {
+                    count.wrappedValue -= 1
+                    teamCount.wrappedValue -= 1
+                }
+            }) {
+                Image(systemName: "minus.circle")
             }
+            .buttonStyle(PlainButtonStyle())
+            .background(color)
+
+            VStack(alignment: .leading) {
+                Text("\(player.name)").font(.title)
+                Text("Score: \(count.wrappedValue)").font(.headline)
+            }
+
+            Button(action: {
+                count.wrappedValue += 1
+                teamCount.wrappedValue += 1
+            }) {
+                Image(systemName: "plus.circle")
+            }
+            .buttonStyle(PlainButtonStyle())
+            .background(color)
+        }
+    }
+ 
+    func loadTeama() {
+        guard let url = URL(string: "https://footyappdev.richardbignell.co.uk/teama.txt") else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else { return }
 
             if let fileContents = String(data: data, encoding: .utf8) {
                 let lines = fileContents.components(separatedBy: "\n")
-                // update UI on the main thread 
+                
                 DispatchQueue.main.async {
                     // Update your UI here
                 }
             }
         }.resume()
     }
-    
-    func loadTeamB() {
-        guard let url = URL(string: "https://footyappdev.richardbignell.co.uk/teamb.txt") else {
-            return
-        }
 
+    func loadTeamB() {
+        guard let url = URL(string: "https://footyappdev.richardbignell.co.uk/teamb.txt") else { return }
+        
         URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data else {
-                return
-            }
+            guard let data = data else { return }
 
             if let fileContents = String(data: data, encoding: .utf8) {
                 let lines = fileContents.components(separatedBy: "\n")
                 
-                // update UI on the main thread 
                 DispatchQueue.main.async {
                     // Update your UI here
                 }
@@ -159,16 +138,22 @@ struct ContentView: View {
 }
 
 struct ResetView: View {
+
     @Binding var teamACount: Int
     @Binding var teamBCount: Int
-    
+
+    @Binding var countsA: [Int]
+    @Binding var countsB: [Int]
+
     var body: some View {
         VStack {
             Text("You can reset your scores here.")
-            
+
             Button(action: {
                 self.teamACount = 0
                 self.teamBCount = 0
+                self.countsA = Array(repeating: 0, count: countsA.count)
+                self.countsB = Array(repeating: 0, count: countsB.count)
             }) {
                 Text("Reset")
             }
